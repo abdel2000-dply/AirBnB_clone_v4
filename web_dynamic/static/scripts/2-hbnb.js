1bd55c107fbf47992d3a3b5851bc8231b0188893
zaynOm <ouaziz0626@gmail.com>
2024-02-08 19:04:24 +0100

    task 3: API status

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,28 @@
+$(() => {
+  const url = 'http://0.0.0.0:5001/api/v1/status/';
+  $.getJSON(url, (data) => {
+    if (data.status == 'OK') {
+      $('#api_status').addClass('available');
+    }
+    else {
+      $('#api_status').removeClass('available');
+    }
+  });
+  const amenity_ids = []
+  $('.popover ul li input').change(function() {
+    let id = $(this).attr('data-id');
+    if (this.checked) {
+      amenity_ids.push(id);
+    }
+    else {
+      const idx = amenity_ids.indexOf(id)
+      amenity_ids.splice(idx, 1)
+    }
+    console.log(amenity_ids);
+    const amenity_names = amenity_ids.map((id) => {
+      return $(`.popover ul li input[data-id=${id}]`).attr('data-name');
+    });
+    console.log(amenity_names);
+    $('.amenities h4').text(amenity_names.join(', '));
+  });
+});
