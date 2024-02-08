9f49fbedbeee155a7c4706d962c1e389629cb7cf
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-02-08 17:13:18 +0100

    task 3:  Select some Amenities to be comfortable! + Fix JavaScript and CSS file paths

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,17 +1,19 @@
 $(() => {
   const amenity_ids = []
-  $('.popover ul li input').change(() => {
-    let id = $(this).val($(this).attr('data-id'));
+  $('.popover ul li input').change(function() {
+    let id = $(this).attr('data-id');
     if (this.checked) {
-      amenity_ids.append(id);
+      amenity_ids.push(id);
     }
     else {
       const idx = amenity_ids.indexOf(id)
       amenity_ids.splice(idx, 1)
     }
-    amenity_ids.map((id) => {
-      const name = $(`.popover input=[data-id=${id}]`).attr('data-name');
-      $('.amenities h4').append(name);
+    console.log(amenity_ids);
+    const amenity_names = amenity_ids.map((id) => {
+      return $(`.popover ul li input[data-id=${id}]`).attr('data-name');
     });
+    console.log(amenity_names);
+    $('.amenities h4').text(amenity_names.join(', '));
   });
 });
