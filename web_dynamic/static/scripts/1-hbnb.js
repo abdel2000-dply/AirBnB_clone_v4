2021a9c82c4d0c2e5b639f6a57ff3fa881d2eb97
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-02-08 20:28:26 +0100

    task 3: API status

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -9,11 +9,9 @@ $(() => {
       const idx = amenity_ids.indexOf(id)
       amenity_ids.splice(idx, 1)
     }
-    console.log(amenity_ids);
     const amenity_names = amenity_ids.map((id) => {
       return $(`.popover ul li input[data-id=${id}]`).attr('data-name');
     });
-    console.log(amenity_names);
     $('.amenities h4').text(amenity_names.join(', '));
   });
 });
--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,7 +1,8 @@
 $(() => {
   const url = 'http://0.0.0.0:5001/api/v1/status/';
   $.getJSON(url, (data) => {
-    if (data.status == 'OK') {
+    console.log(data);
+    if (data.status === 'OK') {
       $('#api_status').addClass('available');
     }
     else {
@@ -18,11 +19,9 @@ $(() => {
       const idx = amenity_ids.indexOf(id)
       amenity_ids.splice(idx, 1)
     }
-    console.log(amenity_ids);
     const amenity_names = amenity_ids.map((id) => {
       return $(`.popover ul li input[data-id=${id}]`).attr('data-name');
     });
-    console.log(amenity_names);
     $('.amenities h4').text(amenity_names.join(', '));
   });
 });
